db42f5c1dc145632ce051770554280446544c531
Denis <elspy@mail.ru>
2022-12-11 22:01:42 +0300

    Созданы три файла для семинара 3

--- task_2.java
+++ task_2.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class task_2 {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите размер списка:");
+        int listSize = input.nextInt();
+        List inList = new ArrayList<>();
+        List outList = new ArrayList<>();
+        for (int i = 1; i < listSize + 1; i++){
+            inList.add(i);  // Заполняем список inList
+        }
+        System.out.print("Необработанный список:\t");
+        for (int i = 0; i < inList.size(); i++){
+            System.out.print(inList.get(i)+" ");// Печатаем элементы списка inList
+        }
+        System.out.print("\nОбработанный список:\t");
+        for (int i = 0; i < inList.size(); i++){
+            int temp = (Integer) inList.get(i);
+            if (temp % 2 != 0){             // Проверяем на четность
+                outList.add(temp);          // Заполняем лист outList
+                System.out.print(temp+" "); // Печатаем элементы листа outList
+            }
+        }
+    }
 }
--- task_3.java
+++ task_3.java
@@ -1,3 +1,41 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class task_3 {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите размер списка:");
+        int listSize = input.nextInt();
+        System.out.print("Максимальное значение элементов списка:");
+        int maxListValue = input.nextInt();
+        Random randNumber = new Random();
+        List newList = new ArrayList<>();
+        int min     = 0;
+        int max     = 0;
+        double sum     = 0.0;
+        for (int i = 0; i < listSize; i++){
+            int elem = randNumber.nextInt(1, maxListValue);
+            newList.add(elem);
+        }
+        min = (Integer) newList.get(0);
+        System.out.print("Заданный список: ");
+        for (Object i : newList){
+            int elem = (Integer) i;
+            System.out.print(elem+" ");
+            if (elem < min){
+                min = elem;
+            }
+            if (elem > max){
+                max = elem;
+            }
+            sum = sum+elem;
+        }
+        double middle = sum / listSize;
+        System.out.println("\nМинимальное значение: "+min);
+        System.out.println("Максимальное значение: "+max);
+        System.out.println("\nСумма значений: "+sum);
+        System.out.println("Среднее значение элемента списка: "+middle);
+    }
 }
